b493e6a8a5dad0e9d1d04d171159110cdfb81d99
Alexandru Grigoras <AG-MobileStation>
2020-03-10 23:34:13 +0200

    Refactored function names

--- robot_parser.py
+++ robot_parser.py
@@ -6,14 +6,14 @@ class RobotParser:
     def __init__(self):
         self.rp = urllib.robotparser.RobotFileParser()
 
-    def robot_parse_read(self, link):
+    def read(self, link):
         self.rp.set_url(link + "/robots.txt")
         self.rp.read()
 
-    def robot_parse_url(self, url):
+    def validate_url(self, url):
         return self.rp.can_fetch("*", url)
 
-    def robot_parse_delay(self):
+    def crawl_delay(self):
         try:
             delay_time = self.rp.crawl_delay("*")
             time.sleep(float(delay_time))
