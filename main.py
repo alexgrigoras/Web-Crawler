0a9d361ca66f308fd6d4b984e396dc77dab911d0
Alexandru Grigoras <AG-MobileStation>
2020-03-10 23:31:56 +0200

    Added method to run mapreduce in parallel mode

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from crawler import Crawler
-
+import os
 
 def main():
     urls = ["https://dmoztools.net/", "https://dmoz-odp.org/"]
-    limit = 10
+    limit = 50
+    nr_nodes = 12
 
     c = Crawler(urls, limit)
     c.crawl()
     c.generate_adjacency_list()
-
+    os.system('mpiexec -np ' + str(nr_nodes) + ' python master_worker.py')
 
 if __name__ == "__main__":
     main()
