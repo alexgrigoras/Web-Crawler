c12dce9568d97643c24c3bd50057fbdcf4a2921b
Alexandru Grigoras <AG-MobileStation>
2020-03-11 15:07:27 +0200

    Added reduce and create directory methods

--- map_reduce.py
+++ map_reduce.py
@@ -1,10 +1,13 @@
 import json
+import os
 
 
 class MapReduce:
-    def __init__(self, id):
-        self.id = id
+    def __init__(self, rank):
+        self.id = rank
         self.key_values = dict()
+        self.__create_directory("output/map/")
+        self.__create_directory("output/reduce/")
 
     def map(self, key, value):
         if value not in self.key_values:
@@ -16,10 +19,25 @@ class MapReduce:
                 temp_list.append(key)
                 self.key_values[value] = temp_list
 
-    def reduce(self):
-        print("[" + str(self.id) + "] > Running reducer")
+    def reduce(self, key, value):
+        file_name_raw = "output/reduce/" + key + ".txt"
+        file_name = file_name_raw.replace('/', '_')
+        file_name = file_name.replace('%', '')
+
+        if os.path.exists(file_name):
+            append_write = 'a'  # append if already exists
+        else:
+            append_write = 'w'  # make a new file if not
+
+        with open(file_name, append_write, encoding='utf-8') as outfile:
+            outfile.writelines(["%s\n" % item for item in value])
 
     def store_values(self):
-        file_name = "output/map-" + str(id) + ".json"
+        file_name = "output/map/map-" + str(self.id) + ".json"
         with open(file_name, 'w+', encoding='utf-8') as outfile:
             json.dump(self.key_values, outfile, ensure_ascii=False, indent=4)
+
+    @staticmethod
+    def __create_directory(path):
+        if not os.path.exists(path):
+            os.makedirs(path)
